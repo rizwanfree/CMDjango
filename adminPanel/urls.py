27d17cf1d8796dcaae896407391a165fb4eb560a
rizwanfree <rizwanfreelance@gmail.com>
2024-07-29 19:18:01 +0500

    city

--- adminPanel/urls.py
+++ adminPanel/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'adminPanel'
+
 urlpatterns = [
     path('', views.Dashboard, name='Home'),
+    path('citylist/', views.CityList, name='citylist'),
 ]
--- adminPanel/views.py
+++ adminPanel/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from .models import City
+
 # Create your views here.
 
 def Dashboard(request):
     return render(request, 'adminPanel/dashboard.html')
+
+
+def CityList(request):
+     cities = City.objects.all()
+
+     context = {
+          'cities': cities,
+     }
+     return render(request, 'adminPanel/citylist.html', context)
